e9c4e00614f1162f917dd58c43f9296f9ce0460b
santarl <rfsjay@gmail.com>
2025-02-03 11:23:41 +0400

    Init

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#![windows_subsystem = "windows"]
+extern crate winapi;
+
+use std::env;
+use std::str::FromStr;
+use winapi::um::winuser::{SendInput, INPUT, MOUSEINPUT, INPUT_MOUSE, MOUSEEVENTF_WHEEL};
+
+fn scroll_mouse(amount: i32) {
+    let mouse_input = MOUSEINPUT {
+        dx: 0,
+        dy: 0,
+        mouseData: amount as u32,
+        dwFlags: MOUSEEVENTF_WHEEL,
+        time: 0,
+        dwExtraInfo: 0,
+    };
+
+    let input = INPUT {
+        type_: INPUT_MOUSE,
+        u: unsafe { std::mem::transmute(mouse_input) },
+    };
+
+    unsafe {
+        SendInput(1, &input as *const _ as *mut INPUT, std::mem::size_of::<INPUT>() as i32);
+    }
+}
+
+fn main() {
+    if env::args().len() != 2 {
+        eprintln!("Usage: scroll_mouse <amount>");
+        std::process::exit(1);
+    }
+
+    let arg = env::args().nth(1).unwrap();
+    let amount = i32::from_str(&arg).expect("Please provide a valid integer for the scroll amount.");
+
+    scroll_mouse(amount);
+}
